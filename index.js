5f91da7346f0f17b8d9886e9acc10e64d729b377
shafin90 <mashrafiahnam1@gmail.com>
2023-05-21 06:13:35 -0700

    update functionality done

--- index.js
+++ index.js
@@ -22,74 +22,102 @@ const client = new MongoClient(uri, {
   }
 });
 
+// Declare the array variable outside the run() function
+let array = [];
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
+
+
+
     
-
-
-    const array = [];
     const userCollection = client.db("Carz").collection('collections');
 
-    app.get('/mytoys',async (req,res)=>{
+
+   
+
+
+
+    app.get('/users', async (req, res) => {
+      const cursor = userCollection.find()
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.get('/users/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const user = await userCollection.findOne(query);
+      res.send(user);
+    })
+
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      console.log('new user', user);
+      const result = await userCollection.insertOne(user);
+      array.push(user);
+      res.send(result);
+    });
+
+    app.put('/users/:id', async (req, res) => {
+      const id = req.params.id;
+      const user = req.body;
+      console.log(id, user);
+
+      const filter = { _id: new ObjectId(id) }
+      const options = { upsert: true }
+      const updatedUser = {
+        $set: {
+          price: user.price,
+          available_quantity: user.available_quantity,
+          detail_description: user.detail_description
+        }
+      }
+
+      const result = await userCollection.updateOne(filter, updatedUser, options);
+      res.send(result);
+
+    })
+
+ 
+
+    app.delete('/users/:id', async (req, res) => {
+      const id = req.params.id;
+
+
+
+      console.log('please delete from database', id);
+      const query = { _id: new ObjectId(id) }
+
+
+
+
+      // Remove the item from the array based on _id
+      array = array.filter(item => item._id.toString() !== new ObjectId(id).toString());
+    
+
+    
+
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    })
+
+     // moy toy
+     app.get('/mytoys', async (req, res) => {
+
       res.send(array);
     })
 
-    app.get('/users', async( req, res) => {
-        const cursor = userCollection.find()
-        const result = await cursor.toArray();
-        res.send(result);
-    })
 
-    app.get('/users/:id', async(req, res) => {
-        const id = req.params.id;
-        const query = {_id: new ObjectId(id)}
-        const user = await userCollection.findOne(query);
-        res.send(user);
-    })
 
-    app.post('/users', async(req, res) => {
-        const user = req.body;
-        console.log('new user', user);
-        const result = await userCollection.insertOne(user);
-        array.push(user);
-        res.send(result);
-    });
-
-    app.put('/users/:id', async(req, res) =>{
-        const id = req.params.id;
-        const user = req.body;
-        console.log(id, user);
-        
-        const filter = {_id: new ObjectId(id)}
-        const options = {upsert: true}
-        const updatedUser = {
-            $set: {
-                name: user.name,
-                email: user.email
-            }
-        }
-
-        const result = await userCollection.updateOne(filter, updatedUser, options );
-        res.send(result);
-
-    })
-
-    app.delete('/users/:id', async(req, res) =>{
-        const id = req.params.id;
-        console.log('please delete from database', id);
-        const query = { _id: new ObjectId(id)}
-        
-        const result = await userCollection.deleteOne(query);
-        res.send(result);
-    })
 
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-  
+
     // await client.close();
   }
 }
@@ -97,12 +125,12 @@ run().catch(console.dir);
 
 
 
-app.get('/hi', (req, res) =>{
-    res.send('SIMPLE CRUD IS RUNNING')
+app.get('/hi', (req, res) => {
+  res.send('SIMPLE CRUD IS RUNNING')
 })
 
-app.listen(port, () =>{
-    console.log(`SIMPLE CRUD is running on port, ${port}`)
+app.listen(port, () => {
+  console.log(`SIMPLE CRUD is running on port, ${port}`)
 })
 
 
